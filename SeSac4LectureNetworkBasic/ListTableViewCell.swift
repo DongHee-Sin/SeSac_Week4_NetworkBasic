4e550d93fa28c782369164aeeb38ad4d65bebae8
DongHee-Sin <voslr0601@naver.com>
2022-07-27 12:57:18 +0900

    ☘️ 수업 내용 커밋 1. UIViewController & Xib(cell) 사용하여 TableView 화면 구현 2. Delegate, Datasource Protocol 학습

---
+++ SeSac4LectureNetworkBasic/ListTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ListTableViewCell.swift
+//  SeSac4LectureNetworkBasic
+//
+//  Created by 신동희 on 2022/07/27.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    static let identifier: String = "ListTableViewCell"
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
---
+++ SeSac4LectureNetworkBasic/SearchViewController.swift
@@ -0,0 +1,58 @@
+//
+//  SearchViewController.swift
+//  SeSac4LectureNetworkBasic
+//
+//  Created by 신동희 on 2022/07/27.
+//
+
+import UIKit
+
+/*
+ Swift Protocol
+ - Delegate
+ - Datasource
+ 
+ 1. 왼팔과 오른팔 가져오기
+ 2. TableView Outlet 연결
+ 3. 1+2 : 팔 연결
+*/
+ 
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var searchTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 연결고리 작업 : 테이블뷰가 해야 할 역할 > 뷰컨트롤러에게 요청
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
+        
+        // 테이블뷰가 사용할 테이블뷰 셀(XIB) 등록
+        // XIB : xml interface builder <= Nib
+        searchTableView.register(UINib(nibName: ListTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: ListTableViewCell.identifier)
+    }
+
+    
+}
+
+
+// MARK: - TableView Protocol
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ListTableViewCell.identifier, for: indexPath) as? ListTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.titleLabel.font = .boldSystemFont(ofSize: 22)
+        cell.titleLabel.text = "Hello"
+        
+        return cell
+    }
+    
+    
+}
